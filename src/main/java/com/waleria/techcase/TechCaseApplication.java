8d8a0bd9943d0d6d3d5ac721906b62ae3b89a208
Waleria Oliveira <waleriasantosdeoliveira@gmail.com>
2026-06-30 21:38:34 -0300

    first setup config

---
+++ src/main/java/com/waleria/techcase/TechCaseApplication.java
@@ -0,0 +1,13 @@
+package com.waleria.techcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechCaseApplication.class, args);
+    }
+
+}
